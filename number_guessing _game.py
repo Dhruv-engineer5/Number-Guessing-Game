3001cf1f04becbb71ff4ff7bf45815266cb4930b
Dhruv Ghodasara <dhruvaghodasara@gmail.com>
2026-07-19 12:39:16 +0530

    Improved number guessing game

--- number_guessing _game.py
+++ number_guessing _game.py
@@ -1,17 +1,37 @@
 import random
 
-print("Hi,Welcome to Number Guessing Game!")
+File = "History.txt"
+def history():
+    file = open(File , 'r')
+    lines = file.readline()
+    if len == 0 :
+        print("Nothing")
+        return
+    else:
+        print(file.read)
 
+def Add():
+    file = open(File , 'a')
+    file.write("com's guess:"+ str(secret_number) + '\n')
+    file.write("Your guess:"+ str(guesses) + '\n''\n')
+
+print("Hi,Welcome to Number Guessing Game!")
 secret_number = random.randint(1, 100)
 guesses = []
 
 while True:
+
     guess = int(input("Enter a number (1-100): "))
     guesses.append(guess)
 
     if(guess == secret_number):
         print("Congratulations! You guessed correctly.")
         print(f"You guessed it in {len(guesses)} attempts.")
+        Add()
+
+        i = input("Do you want to see history! y/n").strip().lower()
+        if i == "y":
+            history()
         break
 
     elif(guess < secret_number):
